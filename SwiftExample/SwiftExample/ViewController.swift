dc8533b5d7aa814b6994555025518fdf0d74a16b
Jason Pell <jason@adnuntius.com>
2022-07-25 17:37:02 +1000

    enable updating the view via new sdk api

--- SwiftExample/SwiftExample/ViewController.swift
+++ SwiftExample/SwiftExample/ViewController.swift
@@ -9,30 +9,33 @@ import AdnuntiusSDK
 import WebKit
 import UIKit
 
-class ViewController: UIViewController, AdLoadCompletionHandler, AdnSdkHandler {
-    private let globalUserId = UserDefaults.standard.string(forKey: "globalUserId")!
-    private let sessionId = UUID().uuidString
+class ViewController: UIViewController, LoadAdHandler {
+    private var scrollView: UIScrollView?
     
     private var adView: AdnuntiusAdWebView = {
         let view = AdnuntiusAdWebView()
         view.logger.id = "adView"
-        view.logger.debug = false
+        view.logger.debug = true
+        view.logger.verbose = false
         view.heightAnchor.constraint(equalToConstant: 160).isActive = true
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor.gray
         view.scrollView.isOpaque = false
+        view.scrollView.isScrollEnabled = false
         view.isOpaque = false
         return view
     }()
-    
+
     private var adView2: AdnuntiusAdWebView = {
         let view = AdnuntiusAdWebView()
         view.logger.id = "adView2"
-        view.logger.debug = false
+        view.logger.debug = true
+        view.logger.verbose = false
         view.heightAnchor.constraint(equalToConstant: 160).isActive = true
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = UIColor.blue
         view.scrollView.isOpaque = false
+        view.scrollView.isScrollEnabled = false
         view.isOpaque = false
         return view
     }()
@@ -40,12 +43,12 @@ class ViewController: UIViewController, AdLoadCompletionHandler, AdnSdkHandler {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let scrollView: UIScrollView = {
+        self.scrollView = {
             let view = UIScrollView()
             view.translatesAutoresizingMaskIntoConstraints = false
             return view
         }()
-        scrollView.delegate = self
+        self.scrollView!.delegate = self
         
         let scrollStackViewContainer: UIStackView = {
             let view = UIStackView()
@@ -72,31 +75,22 @@ class ViewController: UIViewController, AdLoadCompletionHandler, AdnSdkHandler {
         }()
         
         let margins = view.layoutMarginsGuide
-        view.addSubview(scrollView)
-        scrollView.addSubview(scrollStackViewContainer)
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
-        scrollStackViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        scrollStackViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        scrollStackViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        scrollStackViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        scrollStackViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        view.addSubview(self.scrollView!)
+        self.scrollView!.addSubview(scrollStackViewContainer)
+        self.scrollView!.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        self.scrollView!.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        self.scrollView!.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+        self.scrollView!.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+        scrollStackViewContainer.leadingAnchor.constraint(equalTo: self.scrollView!.leadingAnchor).isActive = true
+        scrollStackViewContainer.trailingAnchor.constraint(equalTo: self.scrollView!.trailingAnchor).isActive = true
+        scrollStackViewContainer.topAnchor.constraint(equalTo: self.scrollView!.topAnchor).isActive = true
+        scrollStackViewContainer.bottomAnchor.constraint(equalTo: self.scrollView!.bottomAnchor).isActive = true
+        scrollStackViewContainer.widthAnchor.constraint(equalTo: self.scrollView!.widthAnchor).isActive = true
         
         scrollStackViewContainer.addArrangedSubview(labelAbove)
         scrollStackViewContainer.addArrangedSubview(adView)
         scrollStackViewContainer.addArrangedSubview(labelBetween)
         scrollStackViewContainer.addArrangedSubview(adView2)
-        
-        let defaults = UserDefaults.standard
-        defaults.register(defaults:["globalUserId" : ""])
-
-        let globalUserId = defaults.string(forKey: "globalUserId")!
-        if globalUserId.isEmpty {
-            print("No user id found, generating and saving a new one")
-            defaults.set(UUID().uuidString, forKey: "globalUserId")
-        }
     }
 
     private func promptToLoadAd() {
@@ -105,10 +99,6 @@ class ViewController: UIViewController, AdLoadCompletionHandler, AdnSdkHandler {
         let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
             print("Ok button tapped")
             self.loadFromConfig()
-            //var urlRequest: URLRequest = URLRequest(url: URL(string: "http://localhost:9999/observer.html")!)
-            //self.adView.load(urlRequest)
-            //var urlRequest2: URLRequest = URLRequest(url: URL(string: "http://localhost:9999/observer2.html")!)
-            //self.adView2.load(urlRequest2)
         })
         
         let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
@@ -131,50 +121,18 @@ class ViewController: UIViewController, AdLoadCompletionHandler, AdnSdkHandler {
     }
  
     private func loadFromConfig() {
-        
-        //print("The global user id is \(globalUserId)")
-
-        //adView.setEnv(AdnuntiusEnvironment.localhost)
-        //let adRequest = AdRequest("00000000000001d9")
-        
         let adRequest = AdRequest("000000000006f450")
         adRequest.keyValue("version", "6s")
-        adRequest.height("10000")
-        adRequest.width("10000")
-        adRequest.sessionId(sessionId)
-        adRequest.userId(globalUserId)
-        //adRequest.livePreview("7pmy5r9rj62fyhjm", "60n8zsv29kx9mmty")
         adRequest.useCookies(false)
-        
-        adView.loadAd(adRequest, completionHandler: self, adnSdkHandler: self)
+        adView.loadAd(adRequest, self)
         
         let adRequest2 = AdRequest("000000000006f450")
         adRequest2.keyValue("version", "X")
-        adRequest2.height("10000")
-        adRequest2.width("10000")
-        adRequest2.sessionId(sessionId)
-        adRequest2.userId(globalUserId)
-        //adRequest.livePreview("7pmy5r9rj62fyhjm", "60n8zsv29kx9mmty")
         adRequest2.useCookies(false)
-        adView2.loadAd(adRequest2, completionHandler: self, adnSdkHandler: self)
+        adView2.loadAd(adRequest2, self)
     }
     
-    func onNoAdResponse(_ view: AdnuntiusAdWebView) {
-        print("No Ad Found!")
-        self.adView.isHidden = true
-    }
-    
-    func onFailure(_ view: AdnuntiusAdWebView, _ message: String) {
-        view.loadHTMLString("<h1>Error is: \(message)</h1>",
-        baseURL: nil)
-    }
-
-    func onAdResponse(_ view: AdnuntiusAdWebView, _ width: Int, _ height: Int) {
-        //print("onAdResponse \(view.logger.id): width: \(width), height: \(height)")
-    }
-    
-    func onClose(_ view: AdnuntiusAdWebView) {
-        print("Here is close")
-        UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
+    func onAdResponse(_ view: AdnuntiusAdWebView, _ response: AdResponseInfo) {
+        print("onAdResponse: \(response)")
     }
 }
